ad7bd03cc59dc461f22a2a34da8d37610cea0bfe
Emile0001 <boteseh@gmail.com>
2024-07-04 09:42:57 +0200

    refactor

--- src/app/assignmentDirectives/assignmentDirectives.component.ts
+++ src/app/assignmentDirectives/assignmentDirectives.component.ts
@@ -1,29 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-assignmentDirective',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './assignmentDirectives.component.html',
   styleUrl: './assignmentDirectives.component.scss',
 })
 export class AssignmentDirectives {
-  visibilityStatus: string = 'hidden';
-  incrementMessage: number = 0;
-  logCounter: number[] = [];
-  logTime: Date[] = [];
+  // visibilityStatus: string = 'hidden';
+  // incrementMessage: number = 0;
+  // logCounter: number[] = [];
+  // logTime: Date[] = [];
+  // incrementCounter() {
+  //   if (this.visibilityStatus === 'visible') {
+  //     this.incrementMessage += 1;
+  //     this.logCounter.push(this.incrementMessage);
+  //     this.logTime.push(new Date());
+  //   }
+  // }
+  // onToggleVisibility() {
+  //   this.visibilityStatus =
+  //     this.visibilityStatus === 'hidden' ? 'visible' : 'hidden';
+  //   this.incrementCounter();
+  // }
 
-  incrementCounter() {
-    if (this.visibilityStatus === 'visible') {
-      this.incrementMessage += 1;
-      this.logCounter.push(this.incrementMessage);
-      this.logTime.push(new Date());
-    }
-  }
+  isVisible: boolean;
+  logNumber: number[] = [];
+  logDate: Date[] = [];
 
-  onToggleVisibility() {
-    this.visibilityStatus =
-      this.visibilityStatus === 'hidden' ? 'visible' : 'hidden';
-    this.incrementCounter();
+  constructor(logNumber: number[]) {}
+
+  displayMessage() {}
+  logDetails() {}
+
+  onItemEntered() {
+    this.logDetails();
+    setTimeout(() => {}, 3000);
   }
 }
